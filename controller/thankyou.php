655bf27d06196c7c6780cfa0df797dff3c1799c8
PMTu285 <heilpmtu@gmail.com>
2024-12-08 17:11:51 +0700

    Delete controller directory

--- controller/thankyou.php
+++
@@ -1,7 +0,0 @@
-<div class="container padding-bottom-3x mb-1 thankyou">
-        <div class="text-center">
-            <h1>Cảm ơn hảo hán đã đặt hàng!</h1>
-            <p>Đơn hàng của bạn đã được ghi nhận và đang chờ thanh toán.</p>
-            <a class="btn btn-primary" href="index.php">Quay lại trang chủ</a>
-        </div>
-    </div>
